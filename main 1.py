6648297a921b43557103a173bc6eccebdc00e1d9
lucassimanihuruk <113275103+lucassimanihuruk@users.noreply.github.com>
2022-09-18 16:38:22 +0700

    Add files via upload

---
+++ main 1.py
@@ -0,0 +1,12 @@
+lucas=input(' ').lower().replace("","")
+x=lucas[0:5]
+if(lucas=='hello'):
+    simanihuruk=0
+elif(lucas=='hello'):
+    simanihuruk=0
+elif(lucas[0]=='h'):
+    simanihuruk=20
+else :
+    simanihuruk=100
+
+print ('$',simanihuruk)
